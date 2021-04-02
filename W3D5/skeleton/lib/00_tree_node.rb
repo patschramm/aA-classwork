a129de1a7725f4f987ed0ad4b66ec94a0ae0dbe2
“dennis” <dennisyu225@gmail.com>
2021-04-02 14:09:33 -0700

    add initialize and parent=

--- W3D5/skeleton/lib/00_tree_node.rb
+++ W3D5/skeleton/lib/00_tree_node.rb
@@ -1,3 +1,20 @@
 class PolyTreeNode
 
+    attr_reader :parent, :children, :value 
+
+    def initialize(value)
+        @value = value 
+        @parent = nil
+        @children = []
+    end
+
+    def parent=(new_node)
+        new_parent = self.parent 
+        if new_node != nil 
+            
+        end
+    end
+
+
+
 end
