b7985af4c504ef4613c5d5709bee9688b88fa0d7
“dennis” <dennisyu225@gmail.com>
2021-04-02 15:10:11 -0700

    complete PolyTreeNode

--- W3D5/skeleton/lib/00_tree_node.rb
+++ W3D5/skeleton/lib/00_tree_node.rb
@@ -16,6 +16,40 @@ class PolyTreeNode
         end
     end
 
-    
+    def add_child(child_node)
+        child_node.parent = self 
+    end
+
+    def remove_child(child_node)
+        if children.include?(child_node)
+            child_node.parent = nil 
+        else
+            raise 
+        end
+    end
+
+    def dfs(target_value)
+        return self if self.value == target_value
+        children.each do |child|
+            current_node = child.dfs(target_value)
+            return current_node if current_node 
+        end
+        nil
+    end
+
+    def bfs(target_value)
+        queue = [self]
+        until queue.empty? 
+            current_node = queue.shift 
+            return current_node if current_node.value == target_value
+            current_node.children.each do |child|
+                queue << child 
+            end
+        end
+        nil 
+    end
+
+
+
 
 end
