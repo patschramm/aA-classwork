2e9eab920da2e52d53af94517718b03d00c3e4d6
“dennis” <dennisyu225@gmail.com>
2021-04-02 16:33:53 -0700

    started KnightPathFinder

--- W3D5/Knight-Travails/KnightPathFinder.rb
+++ W3D5/Knight-Travails/KnightPathFinder.rb
@@ -1,3 +1,19 @@
+require_relative 'PolyTreeNode.rb'
+
 class KnightPathFinder
+
+    def initialize(start_pos)
+        @current_pos = start_pos
+        @root_node = PolyTreeNode.new(start_pos)
+    end
+
+    def build_move_tree
+        
+    end
+
+
+
+
+
 
 end
