d3aa14c3832b47e33b1346b974c17a73ed8ddd18
patschramm <patrickcharlesschramm@gmail.com>
2021-04-05 14:57:08 -0700

    started working on AI, initialized node board

--- W4D1/TicTacToe-AI/lib/tic_tac_toe_node.rb
+++ W4D1/TicTacToe-AI/lib/tic_tac_toe_node.rb
@@ -1,7 +1,12 @@
 require_relative 'tic_tac_toe'
 
 class TicTacToeNode
+  attr_reader :board, :next_mover_mark, :prev_move_pos
+
   def initialize(board, next_mover_mark, prev_move_pos = nil)
+    @board = board
+    @next_mover_mark = next_mover_mark
+    @prev_move_pos = prev_move_pos
   end
 
   def losing_node?(evaluator)
@@ -10,8 +15,13 @@ class TicTacToeNode
   def winning_node?(evaluator)
   end
 
+  def all_empty_spots
+    empty_spots = []
+  end
+
   # This method generates an array of all moves that can be made after
   # the current move.
   def children
+
   end
 end
