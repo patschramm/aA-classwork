6af0fd8d49be8e961313080890478db1e1c38d1a
“dennis” <dennisyu225@gmail.com>
2021-04-05 16:02:18 -0700

    Completed initialize and children

--- W4D1/TicTacToe-AI/lib/tic_tac_toe_node.rb
+++ W4D1/TicTacToe-AI/lib/tic_tac_toe_node.rb
@@ -1,4 +1,5 @@
 require_relative 'tic_tac_toe'
+require 'byebug'
 
 class TicTacToeNode
   attr_reader :board, :next_mover_mark, :prev_move_pos
@@ -10,18 +11,30 @@ class TicTacToeNode
   end
 
   def losing_node?(evaluator)
+
   end
 
   def winning_node?(evaluator)
+
   end
 
-  def all_empty_spots
-    empty_spots = []
-  end
 
   # This method generates an array of all moves that can be made after
   # the current move.
   def children
-
+    children = []
+    (0..2).each do |row_idx|
+        (0..2).each do |col_idx|
+            # debugger 
+            pos = [row_idx, col_idx] 
+            if board.empty?(pos)
+                dupped_board = board.dup 
+                dupped_board[pos] = next_mover_mark
+                next_mover_mark = self.next_mover_mark == :x ? :o : :x
+                children << TicTacToeNode.new(dupped_board, next_mover_mark, pos)
+            end
+        end
+    end
+    children
   end
 end
