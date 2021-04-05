14b7c61816358cea41f701e5e9d50c3760c3a09b
patschramm <patrickcharlesschramm@gmail.com>
2021-04-05 11:08:40 -0700

    added methods to KnightPathFinder

--- W3D5/Knight-Travails/KnightPathFinder.rb
+++ W3D5/Knight-Travails/KnightPathFinder.rb
@@ -1,15 +1,23 @@
 require_relative 'PolyTreeNode.rb'
 
 class KnightPathFinder
+    MOVES = [[2, 1], [-2, 1], [2, -1], [-2, -1], [1, 2], [-1, 2], [1, -2], [-1, -2]]
 
     def initialize(start_pos)
         @current_pos = start_pos
         @root_node = PolyTreeNode.new(start_pos)
+        @
     end
 
     def build_move_tree
-        
+        #building tree
     end
+
+    def self.valid_moves(pos)
+
+    end
+
+
 
 
 
