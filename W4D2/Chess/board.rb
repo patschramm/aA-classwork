aa1914655553a2f0c9c01a31fb843440c57ab258
patschramm <patrickcharlesschramm@gmail.com>
2021-04-06 16:07:51 -0700

    added board methods for movement of pieces and initialization

--- W4D2/Chess/board.rb
+++ W4D2/Chess/board.rb
@@ -1,17 +1,30 @@
 class Board
+    attr_reader :rows
 
     def initialize
-        @rows = Array.new(8) {Array.new(8)}
+        @rows = Array.new(8) {Array.new(8) {Piece.new('')}}
     end
 
     def [](pos)
         x, y = pos
-        @row[x][y]
+        @rows[x][y]
     end
 
     def []=(pos, piece)
         x, y = pos
-        @row[x][y] = piece
+        @rows[x][y] = piece
+    end
+
+    def add_piece(pos, piece)
+        self[pos] = piece
+    end
+
+    def move_piece(start_pos, end_pos)
+        self[start_pos], self[end_pos] = self[end_pos], self[start_pos]
+    end
+
+    def print
+        # print board with piece names
     end
 
 end
