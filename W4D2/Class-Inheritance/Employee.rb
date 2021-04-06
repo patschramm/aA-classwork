025c3d7bbb0ab99cd80e8b644b27cbd1bcef376f
guocong <ryp0313@gmail.com>
2021-04-06 14:14:39 -0700

    Make some change

--- W4D2/Class-Inheritance/Employee.rb
+++ W4D2/Class-Inheritance/Employee.rb
@@ -1,12 +1,19 @@
 class Employee
 
-    attr_reader :salary
+    attr_accessor :salary, :boss
 
     def initialize(name, title, salary, boss = nil)
         @name = name
         @title = title
         @salary = salary
+        self.boss = boss
+    end
+
+    def boss=(boss)
         @boss = boss
+        if boss != nil
+            @boss.add_employees(self)
+        end
     end
 
     def bonus(multiplier)
@@ -16,8 +23,8 @@ end
 
 class Manager < Employee
 
-    def initialize
-        super
+    def initialize(name, title, salary, boss = nil)
+        super(name, title, salary, boss = nil)
         @employees = []
     end
 
@@ -27,7 +34,24 @@ class Manager < Employee
         bonus
     end
 
-    def add_employees
-        
+    def add_employees(employee)
+        @employees << employee 
+    end
+
+    def get_all_employees
+        all_employee = []
+        queue = @employee
+        until queue.empty?
+            queue.first.
+            all_employee << queue.shift
     end
 end
+
+ned = Manager.new("Ned", "Founder", 1000000)
+darren = Manager.new("Darren", "TA Manager", 78000, ned)
+shawan = Employee.new("Shawna", "TA", 12000, darren)
+david = Employee.new("David", "TA", 10000, darren)
+
+p ned.bonus(5) # => 500_000
+p darren.bonus(4) # => 88_000
+p david.bonus(3) # => 30_000
