941d7ca77613b72bc6ec612108f7cb1b6fb1ca13
lefuller <fullernle@gmail.com>
2021-04-08 15:11:36 -0700

    implement specs and code for #my_transpose and #stock_picker

--- W4D4/First-TDD-Projects/lib/TDD.rb
+++ W4D4/First-TDD-Projects/lib/TDD.rb
@@ -18,4 +18,33 @@ class Array
 
         new_arr
     end
+
+    def my_transpose
+      transposed = self.dup 
+
+      length.times do |i|
+        (0...i).each do |j|
+          transposed[i][j] = self[j][i]
+        end
+      end
+
+      transposed
+    end
+
+    def stock_picker 
+      max = [0,0]
+
+
+      (0...length-1).each do |i|
+        (0...length).each do |j|
+          if j > i && self[j] > self[i]
+            diff = self[j] - self[i]
+            max_diff = self[max[1]] - self[max[0]]
+            max = [i,j] if diff > max_diff
+          end
+        end
+      end
+
+      max
+    end
 end
--- W4D4/First-TDD-Projects/spec/TDD_spec.rb
+++ W4D4/First-TDD-Projects/spec/TDD_spec.rb
@@ -1,38 +1,69 @@
 require 'rspec'
 require 'TDD'
 
-describe 'Array#my_uniq' do
+describe 'Array' do 
+  describe '#my_uniq' do
 
-    subject(:arr) { [1, 2, 1, 3, 3] }
-    it 'should remove all duplicates' do
-        # arr = [1, 2, 1, 3, 3]
-        uniq_arr = arr.my_uniq
-        expect(uniq_arr).to contain_exactly(1, 2, 3)
+      subject(:arr) { [1, 2, 1, 3, 3] }
+      it 'should remove all duplicates' do
+          # arr = [1, 2, 1, 3, 3]
+          uniq_arr = arr.my_uniq
+          expect(uniq_arr).to contain_exactly(1, 2, 3)
+      end
+
+      it 'should not modify original array' do
+          arr.my_uniq
+          expect(arr).to eq(arr)
+      end
+
+      it 'should return uniq values in original order' do
+          uniq_arr = arr.my_uniq
+          expect(uniq_arr).to eq([1, 2, 3])
+      end
+
+  end
+
+  describe '#two_sum' do
+
+      subject(:arr) { [-1, 0, 2, -2, 1] }
+      it 'should return indices where elements sum to zero' do
+          indices = arr.two_sum
+          possible_results = [[[2, 3], [0, 4]], [[0, 4], [2, 3]], [[3, 2], [4, 0]], [[4, 0], [3, 2]]]
+          expect(possible_results).to include(indices)
+      end
+
+      it 'should return indices in dictionary order' do
+          indices = arr.two_sum
+          expect(indices).to eq([[0, 4], [2, 3]])
+      end
+  end
+
+  describe "#my_transpose" do 
+    subject(:arr) { [[0,1,2], [3,4,5], [6,7,8]]}
+
+    it 'should transpose rows and columns' do 
+      transposed = arr.my_transpose 
+
+      expect(transposed).to eq(arr.transpose) 
+    end
+  end
+
+  describe "#stock_picker" do 
+    subject(:stonks) { [100, 300, 400, 100, 500, 800, 50] }
+
+    it "should return the best pair of days for buying and selling stock" do 
+      expect(stonks.stock_picker).to contain_exactly(0,5)
     end
 
-    it 'should not modify original array' do
-        arr.my_uniq
-        expect(arr).to eq(arr)
+    it "should not sell stock before buying it" do 
+      expect(stonks.stock_picker).not_to contain_exactly(6,5)
+      expect(stonks.stock_picker).not_to be_falsey
     end
 
-    it 'should return uniq values in original order' do
-        uniq_arr = arr.my_uniq
-        expect(uniq_arr).to eq([1, 2, 3])
+    it "should return an array of 2 elements" do 
+      expect(stonks.stock_picker.length).to eq(2)
     end
+  end
 
 end
 
-describe 'Array#two_sum' do
-
-    subject(:arr) { [-1, 0, 2, -2, 1] }
-    it 'should return indices where elements sum to zero' do
-        indices = arr.two_sum
-        possible_results = [[[2, 3], [0, 4]], [[0, 4], [2, 3]], [[3, 2], [4, 0]], [[4, 0], [3, 2]]]
-        expect(possible_results).to include(indices)
-    end
-
-    it 'should return indices in dictionary order' do
-        indices = arr.two_sum
-        expect(indices).to eq([[0, 4], [2, 3]])
-    end
-end
