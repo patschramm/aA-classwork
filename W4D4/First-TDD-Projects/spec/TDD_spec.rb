7278d4cd3b10d072157c1754cfceaf5c2bf4d011
patschramm <patrickcharlesschramm@gmail.com>
2021-04-08 13:51:00 -0700

    made gemfile .gitignore

--- W4D4/First-TDD-Projects/spec/TDD_spec.rb
+++ W4D4/First-TDD-Projects/spec/TDD_spec.rb
@@ -1,2 +1,10 @@
 require 'rspec'
 require 'TDD'
+
+describe 'Array#uniq' do
+    it 'should remove all duplicates' do
+        arr = [1, 2, 1, 3, 3]
+        uniq_arr = arr.uniq
+        expect(uniq_arr).to eq([1, 2, 3])
+    end
+end
