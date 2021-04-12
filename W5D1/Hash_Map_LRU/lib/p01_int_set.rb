f014760732bbaa622583eac9ebd631f07a43217a
patschramm <patrickcharlesschramm@gmail.com>
2021-04-12 14:28:48 -0700

    worked on MaxIntSet and IntSet classes

--- W5D1/Hash_Map_LRU/lib/p01_int_set.rb
+++ W5D1/Hash_Map_LRU/lib/p01_int_set.rb
@@ -1,14 +1,22 @@
 class MaxIntSet
+  attr_reader :store
+
   def initialize(max)
+    @max = max
+    @store = Array.new(max, false)
   end
 
   def insert(num)
+    raise 'Out of bounds' if @max <= num || num < 0
+    @store[num] = true
   end
 
   def remove(num)
+    @store[num] = false
   end
 
   def include?(num)
+    @store[num] == true
   end
 
   private
@@ -27,18 +35,21 @@ class IntSet
   end
 
   def insert(num)
+    self[num] << num if !include?(num)
   end
 
   def remove(num)
+    self[num].delete(num) if include?(num)
   end
 
   def include?(num)
+    self[num].include?(num)
   end
 
   private
 
   def [](num)
-    # optional but useful; return the bucket corresponding to `num`
+    @store[num % @store.length]
   end
 
   def num_buckets
