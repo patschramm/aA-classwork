3ca27c2f40f07e07a2659bd3137b1bdd3e753d70
patschramm <patrickcharlesschramm@gmail.com>
2021-04-13 16:45:24 -0700

    finished 04 and started 05 in SQL_Zoo

--- W5D2/SQL_Zoo/lib/05_aggregates.rb
+++ W5D2/SQL_Zoo/lib/05_aggregates.rb
@@ -22,24 +22,46 @@ end
 def continents
   # List all the continents - just once each.
   execute(<<-SQL)
+  SELECT DISTINCT
+    continent
+  FROM
+    countries
   SQL
 end
 
 def africa_gdp
   # Give the total GDP of Africa.
   execute(<<-SQL)
+  SELECT
+    SUM(gdp) AS gdp_africa
+  FROM
+    countries
+  WHERE
+    continent = 'Africa'
   SQL
 end
 
 def area_count
   # How many countries have an area of more than 1,000,000?
   execute(<<-SQL)
+  SELECT
+    COUNT(*)
+  FROM
+    countries
+  WHERE
+    area > 1000000
   SQL
 end
 
 def group_population
   # What is the total population of ('France','Germany','Spain')?
   execute(<<-SQL)
+  SELECT
+    SUM(population)
+  FROM
+    countries
+  WHERE
+    name IN ('France','Germany','Spain')
   SQL
 end
 
