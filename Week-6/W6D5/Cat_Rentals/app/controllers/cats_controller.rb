6a284e0a592b6dd6bb2222f18df8f7b84eac03a5
Michael Lau <lau.michael2@gmail.com>
2021-04-23 16:46:52 -0700

    Create form

--- Week-6/W6D5/Cat_Rentals/app/controllers/cats_controller.rb
+++ Week-6/W6D5/Cat_Rentals/app/controllers/cats_controller.rb
@@ -3,4 +3,20 @@ class CatsController < ApplicationController
         @cats = Cat.all
         render :index
     end
+
+    def show
+        @cat = Cat.find_by(id: params[:id])
+        render :show
+    end
+
+    def new
+        @cat = Cat.new
+        render :new
+    end
+
+    private
+    def cat_params
+        params.require(:cat).permit(:name, :color, :birth_date, :sex, :description)
+    end
+
 end
