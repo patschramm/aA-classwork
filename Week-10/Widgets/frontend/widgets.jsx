4aca7807d55515af4a22c4dc7435669bcf077c04
patschramm <>
2021-05-18 14:19:24 -0700

    started new widgets project

---
+++ Week-10/Widgets/frontend/widgets.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Root from "../root";
+
+document.addEventListener("DOMContentLoaded", function () {
+    ReactDOM.render(<Root/>, document.getElementById("main"));
+});
---
+++ Week-10/Widgets/root.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Root = function () {
+        return (
+            <div>React is working</div>
+        );
+};
+
+export default Root;
